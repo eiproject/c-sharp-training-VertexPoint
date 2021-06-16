6df4bdc6465b6111ca4a6ebc57f48f636e3c3e8b
Kosmas Suryopranoto <kosmas@formulatrix.com>
2021-06-16 18:38:14 +0700

    bismillah

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TrainingAccessModifier
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      VertexQuadratic V1 = new VertexQuadratic(1, 0, 25);
+      Console.WriteLine(V1.vertexPoint);
+    }
+  }
+}
+
+// namespace Vertex{
+//   class runVertex{
+//     static void  countVertexPoint(){
+//       VertexQuadratic V1 = new VertexQuadratic(1, 1, 1);
+//     }
+    
+    
+//   }
+// }
---
+++ vertexPoint.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TrainingAccessModifier{
+  internal class VertexQuadratic{
+    internal static float[] vertexPoint;
+    internal float h = 0F;
+    internal float k = 0F;  
+    internal VertexQuadratic(int a, int b, int c){
+      h = -b / (2*a);
+      k = a*h*h + b*h + c;
+
+      Console.Write(h);
+      Console.Write(" ");
+      Console.Write(k);
+
+      vertexPoint = new float[]{h, k};
+    }
+  }
+}
